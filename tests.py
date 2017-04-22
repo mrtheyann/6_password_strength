41b5e3827bad9535d7dc1168eded168f759312d0
mrtheyann <darklodr@gmail.com>
2017-04-22 13:05:49 +0500

    fix: strong passwords

--- tests.py
+++ tests.py
@@ -19,7 +19,7 @@ class QuadraticEquationTestCase(unittest.TestCase):
         '''Test if password is uppercase'''
         self.assertEqual(gps('UPPERCASE'), 3)
         self.assertEqual(gps('FOOBARBAZ'), 3)
-    
+
     def test_date(self):
         '''Test if there is a date in password'''
         self.assertEqual(gps('dude1998'), 4)
@@ -29,17 +29,18 @@ class QuadraticEquationTestCase(unittest.TestCase):
         '''Test if password is number-only'''
         self.assertEqual(gps('1337322'), 3)
         self.assertEqual(gps('17291729'), 3)
-    
+
     def test_medium(self):
         '''Test medium strength passwords'''
         self.assertEqual(gps('IAmTheKing'), 4)
         self.assertEqual(gps('W@porize'), 5)
-    
+        self.assertEqual(gps('T1m31992'), 5)
+
     def test_strong(self):
         '''Test strong passwords'''
-        self.assertEqual(gps('T1m31992'), 7)
+        self.assertEqual(gps('T1m3l3ss'), 8)
         self.assertEqual(gps('Ub3rP@sswrd'), 10)
-        self.assertEqual(gps('Cl@rify1'), 10)
+        self.assertEqual(gps('Cl@rify1'), 7)
 
 
 if __name__ == '__main__':
